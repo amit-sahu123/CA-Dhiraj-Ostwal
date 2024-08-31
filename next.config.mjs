522145e7f001c6334d17c023ff09c79297a8e59f
amit sahu <sahuamit8412@gmail.com>
2024-08-31 11:21:14 +0530

    update navbar

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,26 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+//     images: {
+//       domains: ['cloud.appwrite.io'], 
+//     },
+// };
+
+// export default nextConfig;
+
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const bundleAnalyzerConfig = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['cloud.appwrite.io'], // Add more domains if needed
-    },
+  reactStrictMode: true,
+  images: {
+    domains: ['cloud.appwrite.io'], 
+  },
 };
 
-export default nextConfig;
+export default bundleAnalyzerConfig(nextConfig);
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,10 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import { YouTube } from '@mui/icons-material';
-import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Offcanvas from './Offcanvas';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-
 
 export default function Navbar() {
     const [insightsAnchorEl, setInsightsAnchorEl] = useState(null);
@@ -35,9 +33,6 @@ export default function Navbar() {
     };
     const [isHovered, setIsHovered] = useState(false);
     const [isHovered1, setIsHovered1] = useState(false);
-
-   
-   
 
     return (
         <>
@@ -69,25 +64,24 @@ export default function Navbar() {
                     <div className={styles.data}>
                         <ul className="nav">
                             <li className="nav-item">
-                                <a href="/">Home</a>
+                                <Link href="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a 
-                                aria-controls={openServices ? 'services-menu' : undefined}
-                                aria-haspopup="true"
-                                aria-expanded={openServices ? 'true' : undefined}
-                                onClick={handleServicesClick}
-                                onMouseEnter={() => setIsHovered(true)}  
-                                    onMouseLeave={() => setIsHovered(false)} 
+                                <a
+                                    aria-controls={openServices ? 'services-menu' : undefined}
+                                    aria-haspopup="true"
+                                    aria-expanded={openServices ? 'true' : undefined}
+                                    onClick={handleServicesClick}
+                                    onMouseEnter={() => setIsHovered(true)}
+                                    onMouseLeave={() => setIsHovered(false)}
                                     style={{
                                         color: isHovered ? 'orangered' : 'rgb(23, 22, 22)',
-                                        
                                     }}
                                 >
-                                   Services
-                                   <ArrowDropDownIcon />
+                                    Services
+                                    <ArrowDropDownIcon />
                                 </a>
-                                
+
                                 <Menu
                                     id="services-menu"
                                     anchorEl={servicesAnchorEl}
@@ -97,49 +91,45 @@ export default function Navbar() {
                                         'aria-labelledby': 'services-button',
                                     }}
                                 >
-                                    <MenuItem onClick={handleServicesClose} >
-                                        <a href="/services/tax_services" className={styles.menudata} >Tax Services</a>
+                                    <MenuItem onClick={handleServicesClose}>
+                                        <Link href="/services/tax_services" className={styles.menudata}>Tax Services</Link>
                                     </MenuItem>
                                     <MenuItem onClick={handleServicesClose}>
-                                        <a href="/services/company_formation_reg" className={styles.menudata} >Company Formation and Registration</a>
+                                        <Link href="/services/company_formation_reg" className={styles.menudata}>Company Formation and Registration</Link>
                                     </MenuItem>
                                     <MenuItem onClick={handleServicesClose}>
-                                        <a href="/services/auditservices" className={styles.menudata} >Audit Services</a>
+                                        <Link href="/services/auditservices" className={styles.menudata}>Audit Services</Link>
                                     </MenuItem>
                                     <MenuItem onClick={handleServicesClose}>
-                                        <a href="/services/drafting_agreements" className={styles.menudata} > Drafting and Agreements</a>
+                                        <Link href="/services/drafting_agreements" className={styles.menudata}>Drafting and Agreements</Link>
                                     </MenuItem>
                                     <MenuItem onClick={handleServicesClose}>
-                                        <a href="/services/appeals_litigations" className={styles.menudata} >Appeals and Litigations</a>
+                                        <Link href="/services/appeals_litigations" className={styles.menudata}>Appeals and Litigations</Link>
                                     </MenuItem>
                                     <MenuItem onClick={handleServicesClose}>
-                                        <a href="/services/advisory_services" className={styles.menudata} >Advisory Services</a>
+                                        <Link href="/services/advisory_services" className={styles.menudata}>Advisory Services</Link>
                                     </MenuItem>
                                     <MenuItem onClick={handleServicesClose}>
-                                        <a href="/services/other_services" className={styles.menudata} >Other Services</a>
+                                        <Link href="/services/other_services" className={styles.menudata}>Other Services</Link>
                                     </MenuItem>
                                 </Menu>
                             </li>
                             <li className="nav-item">
-                            
                                 <a
                                     aria-controls={openInsights ? 'insights-menu' : undefined}
                                     aria-haspopup="true"
                                     aria-expanded={openInsights ? 'true' : undefined}
                                     onClick={handleInsightsClick}
-                                    onMouseEnter={() => setIsHovered1(true)}  
-                                    onMouseLeave={() => setIsHovered1(false)} 
+                                    onMouseEnter={() => setIsHovered1(true)}
+                                    onMouseLeave={() => setIsHovered1(false)}
                                     style={{
                                         color: isHovered1 ? 'orangered' : 'rgb(23, 22, 22)',
-                                        
                                     }}
                                 >
                                     Insights
                                     <ArrowDropDownIcon />
                                 </a>
 
-
-                                
                                 <Menu
                                     id="insights-menu"
                                     anchorEl={insightsAnchorEl}
@@ -149,35 +139,29 @@ export default function Navbar() {
                                         'aria-labelledby': 'insights-button',
                                     }}
                                 >
-
-                                    <MenuItem onClick={handleInsightsClose} >
-                                        <a href="/profile" className={styles.menudata} >Profile</a>
-                                    </MenuItem>
-
                                     <MenuItem onClick={handleInsightsClose}>
-                                        <a href="/achievementsmore" className={styles.menudata} >Achievements</a>
+                                        <Link href="/profile" className={styles.menudata}>Profile</Link>
                                     </MenuItem>
-                                  
-                                  
                                     <MenuItem onClick={handleInsightsClose}>
-                                        <a href="/reviews" className={styles.menudata}>Reviews</a>
+                                        <Link href="/achievementsmore" className={styles.menudata}>Achievements</Link>
+                                    </MenuItem>
+                                    <MenuItem onClick={handleInsightsClose}>
+                                        <Link href="/reviews" className={styles.menudata}>Reviews</Link>
                                     </MenuItem>
                                 </Menu>
                             </li>
 
                             <li className="nav-item">
-                                <a href="/gallerydetail" >Gallery</a>
+                                <Link href="/gallerydetail">Gallery</Link>
                             </li>
 
                             <li className="nav-item">
-                                <a href="/contact" >Contact</a>
+                                <Link href="/contact">Contact</Link>
                             </li>
 
                             <li className="nav-item">
-                                <a href="/career" >Career</a>
+                                <Link href="/career">Career</Link>
                             </li>
-                           
-                        
                         </ul>
                     </div>
                 </div>
