f77a27e7c1015d79b3bace619ed0d192f2a79ff1
amit sahu <sahuamit8412@gmail.com>
2024-09-06 12:32:07 +0530

    update build file

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,17 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     reactStrictMode: true,
-//     images: {
-//       domains: ['cloud.appwrite.io'], 
-//     },
-// };
-
-// export default nextConfig;
-
 import withBundleAnalyzer from '@next/bundle-analyzer';
 
 const bundleAnalyzerConfig = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
-
+// ----------------------------------------
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cloud.appwrite.io'], 
   },
+  // output: 'export', 
+
 };
 
 export default bundleAnalyzerConfig(nextConfig);
