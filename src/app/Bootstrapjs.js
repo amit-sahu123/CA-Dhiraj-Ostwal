2035c7814011a2633f923d31be716d0dde19643e
amit sahu <sahuamit8412@gmail.com>
2024-09-10 11:59:49 +0530

    update

--- src/app/Bootstrapjs.js
+++ src/app/Bootstrapjs.js
@@ -11,6 +11,8 @@ export default function Bootstrapjs() {
   )
 }
 
+ // "react-slick": "^0.28.1",
+    // "slick-carousel": "^1.8.1"
 // useEffect(() => {
   //   if(typeof window !== "undefined"){
   //     require('bootstrap/dist/js/bootstrap.min.js')
