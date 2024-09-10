b80af9c2b66bbd24f189cfb1d403ca2b5b9e1e99
amit sahu <sahuamit8412@gmail.com>
2024-09-10 12:04:09 +0530

    new update

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,6 @@ export default function Home() {
         <About/>
         <Achievements/>
         <Youtube/>
-        {/* <Employee/>  */}
         <GalleryContainer/>  
         <Award/> 
         <FollowUs/>   
